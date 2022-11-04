8f3fc8b197f1805764c8f2461f008ffbff7c538d
Sierra Green <sierragreen379@gmail.com>
2022-11-03 23:16:37 -0600

    Started on moving the pathCharacter

--- main.js
+++ main.js
@@ -10,10 +10,28 @@ const isGameFinished = false;
 class Field {
     constructor(field) {
         this._field = field;
+        this._x = 0;
+        this._y = 0;
     }
 
     get field() {
         return this._field;
+    }
+
+    get y() {
+        return this._y;
+    }
+
+    get x() {
+        return this._x;
+    }
+
+    set y(value) {
+        this._y += value;
+    }
+
+    set x(value) {
+        this._x += value;
     }
 
     print() {
@@ -28,24 +46,41 @@ const myField = new Field([
     [fieldCharacter, hat, fieldCharacter]
 ]);
 
+myField.print();
+// let theField = myField.field.join();
+// console.log(theField);
+// console.log(myField.field[0][0]);
+
 while (!isGameFinished) {
-    myField.print();
     const direction = prompt('Which direction? u, d, l, or r: ');
     switch (direction) {
         case "u":
             console.log("up!");
+            // move pathCharater
+            myField.print();
             break;
         case "d":
             console.log("down!");
+            // move pathCharater
+            // how to I call a setter and use it????????????
+            myField.y(1);
+            myField.field[myField.y][myField.x] = pathCharacter;
+            myField.print();
             break;
         case "l":
             console.log("left!");
+            // move pathCharater
+            myField.print();
             break;
         case "r":
             console.log("right!");
+            // move pathCharater
+            myField.field[0][1] = pathCharacter;
+            myField.print();
             break;
         default:
             prompt("Enter a valid direction: ");
+            // Need to run through switch statement again with this direction
             break;
     }
 }
